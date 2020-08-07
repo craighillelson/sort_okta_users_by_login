dcad53476ec35b5e6d4458bf1938495811f18466
craighillelson <craighillelson@gmail.com>
2020-08-07 00:20:50 -0400

    update sort_okta_users_by_login.py
    edited doc string, simplified code

--- sort_okta_users_by_login.py
+++ sort_okta_users_by_login.py
@@ -1,4 +1,6 @@
-""" __doc__ """
+"""
+Open 'OktaPasswordHealth.csv' and sort users by last login in descending order.
+"""
 
 import csv
 import re
@@ -13,7 +15,6 @@ OUT_HEADERS = [
     "last_login",
     ]
 
-# open file, populate a dictionary with last login timestamp and login name
 with open("OktaPasswordHealth.csv") as f:
     F_CSV = csv.reader(f)
     HEADERS = [re.sub('[^a-zA-Z_]', '_', h) for h in next(F_CSV)]
@@ -25,21 +26,16 @@ with open("OktaPasswordHealth.csv") as f:
 
 print(RTN())
 
-# output users sorted by last login
 for last_login, login in sorted(USERS.items(), reverse=True):
     print(last_login, login)
 
-# write users and last logins to csv
-with open("users_sorted_by_last_login.csv", "w") as out_file:
+file_name = "users_sorted_by_last_login.csv"
+
+with open(file_name, "w") as out_file:
     OUT_CSV = csv.writer(out_file)
     OUT_CSV.writerow(OUT_HEADERS)
     for last_login, login in sorted(USERS.items(), reverse=True):
         keys_values = (login, last_login)
         OUT_CSV.writerow(keys_values)
 
-print(RTN())
-
-# update user
-print("'users_sorted_by_last_login.csv' exported successfully")
-
-print(RTN())
+print(f"\n{file_name} exported successfully\n")
